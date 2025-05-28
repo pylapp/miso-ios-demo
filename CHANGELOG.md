0b7d4be4c0f765da324c60d2e00aad2a5a7a28c1
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-05-28 14:03:02 +0200

    feat: add hyperlinks in about screen
    - Add hyperlinks to source code, issues board and design system specifications
    - Remove typo in legals
    - Update Package.resolved
    
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- [DesignToolbox] Hyperlinks to design system, source code and issues board in about page
 - [DesignToolbox] Copy/paste build details from about page (Orange-OpenSource/ouds-ios#677)
 
 ### Fixed
--- DesignToolbox/DesignToolbox/Pages/About/AboutPage.swift
+++ DesignToolbox/DesignToolbox/Pages/About/AboutPage.swift
@@ -18,9 +18,14 @@ struct AboutPage: View {
 
     // MARK: Stored properties
 
-    let privacyPolicyUrl: URL
-    let legalInformationUrl: URL
-    let appSettingsUrl: URL
+    private let privacyPolicyUrl: URL
+    private let legalInformationUrl: URL
+    private let appSettingsUrl: URL
+    private let appSourcesUrl: URL
+    private let bugReportUrl: URL
+    private let designSystemUrl: URL
+
+    @Environment(\.layoutDirection) private var layoutDirection
 
     // MARK: Initializer
 
@@ -37,9 +42,24 @@ struct AboutPage: View {
             OL.fatal("Unable to find app settings URL")
         }
 
+        guard let appSourcesUrl = URL(string: "https://github.com/Orange-OpenSource/ouds-ios-design-system-toolbox") else {
+            OL.fatal("Unable to forge app sources URL")
+        }
+
+        guard let bugReportUrl = URL(string: "https://github.com/Orange-OpenSource/ouds-ios/issues/new/choose") else {
+            OL.fatal("Unable to forge bug report URL")
+        }
+
+        guard let designSystemUrl = URL(string: "https://unified-design-system.orange.com") else {
+            OL.fatal("Unable to forge design system URL")
+        }
+
         privacyPolicyUrl = privacyNoticeUrl
         self.legalInformationUrl = legalInformationUrl
         self.appSettingsUrl = appSettingsUrl
+        self.appSourcesUrl = appSourcesUrl
+        self.bugReportUrl = bugReportUrl
+        self.designSystemUrl = designSystemUrl
     }
 
     // MARK: Body
@@ -47,48 +67,90 @@ struct AboutPage: View {
     var body: some View {
         NavigationView {
             List {
-                NavigationLink {
-                    WebView(from: privacyPolicyUrl)
-                        .navigationTitle("app_about_privacyPolicy_label")
-                } label: {
-                    Text("app_about_privacyPolicy_label")
-                }
-
-                NavigationLink {
-                    WebView(from: legalInformationUrl)
-                        .navigationTitle("app_about_legalInformation_label")
-                } label: {
-                    Text("app_about_legalInformation_label")
-                }
-
-                NavigationLink {
-                    AccessibilityStatementPage()
-                        .navigationTitle("app_about_accessibilityStatement_label")
-                } label: {
-                    Text("app_about_accessibilityStatement_label")
-                }
-
-                // TODO: Only for debug purposes, should be displayed in another way
-                DesignToolboxCopyableText("app_about_details_appVersion" <- Bundle.main.marketingVersion, Bundle.main.marketingVersion)
-                DesignToolboxCopyableText("app_about_details_buildNumber" <- Bundle.main.buildNumber, Bundle.main.buildNumber)
-                DesignToolboxCopyableText("app_about_details_buildType" <- Bundle.main.fullBuildType, Bundle.main.fullBuildType)
-                if let buildDetails = Bundle.main.buildDetails {
-                    DesignToolboxCopyableText("app_about_details_githubBuildDetails" <- buildDetails,
-                                              buildDetails.leadingColumnFragment)
-                }
-                if let sdkVersion = Bundle.main.sdkVersion, !sdkVersion.isEmpty {
-                    DesignToolboxCopyableText("app_about_details_sdkVersion" <- sdkVersion, sdkVersion)
-                }
-                DesignToolboxCopyableText("\(Bundle.main.tokensLibraryVersion)",
-                                          Bundle.main.tokensLibraryVersion.leadingColumnFragment)
-
-                Button("app_about_appSettings_label") {
-                    UIApplication.shared.open(appSettingsUrl)
-                }.accessibilityHint("app_about_appSettings_hint_a11y")
+                legalView
+                buildView
+                linksView
             }
             .oudsNavigationTitle("app_bottomBar_about_label")
             .navigationBarTitleDisplayMode(.inline)
         }
         .navigationViewStyle(.stack)
+    }
+
+    // MARK: - Views
+
+    @ViewBuilder
+    private var legalView: some View {
+        NavigationLink {
+            WebView(from: privacyPolicyUrl)
+                .navigationTitle("app_about_privacyPolicy_label")
+        } label: {
+            Text("app_about_privacyPolicy_label")
+        }
+
+        NavigationLink {
+            WebView(from: legalInformationUrl)
+                .navigationTitle("app_about_legalInformation_label")
+        } label: {
+            Text("app_about_legalInformation_label")
+        }
+
+        NavigationLink {
+            AccessibilityStatementPage()
+                .navigationTitle("app_about_accessibilityStatement_label")
+        } label: {
+            Text("app_about_accessibilityStatement_label")
+        }
+    }
+
+    @ViewBuilder
+    private var buildView: some View {
+        // TODO: Only for debug purposes, should be displayed in another way
+        DesignToolboxCopyableText("app_about_details_appVersion" <- Bundle.main.marketingVersion, Bundle.main.marketingVersion)
+        DesignToolboxCopyableText("app_about_details_buildNumber" <- Bundle.main.buildNumber, Bundle.main.buildNumber)
+        DesignToolboxCopyableText("app_about_details_buildType" <- Bundle.main.fullBuildType, Bundle.main.fullBuildType)
+        if let buildDetails = Bundle.main.buildDetails {
+            DesignToolboxCopyableText("app_about_details_githubBuildDetails" <- buildDetails,
+                                      buildDetails.leadingColumnFragment)
+        }
+        if let sdkVersion = Bundle.main.sdkVersion, !sdkVersion.isEmpty {
+            DesignToolboxCopyableText("app_about_details_sdkVersion" <- sdkVersion, sdkVersion)
+        }
+        DesignToolboxCopyableText("\(Bundle.main.tokensLibraryVersion.replacingOccurrences(of: ":", with: ""))",
+                                  Bundle.main.tokensLibraryVersion.leadingColumnFragment)
+    }
+
+    @ViewBuilder
+    private var linksView: some View {
+        Button {
+            UIApplication.shared.open(appSettingsUrl)
+        } label: {
+            HStack {
+                Text("app_about_appSettings_label")
+                Spacer()
+                Image(systemName: "gear").accessibilityHidden(true)
+            }
+        }.accessibilityHint("app_about_appSettings_hint_a11y")
+
+        link(appSourcesUrl, label: "app_about_appSources_label", hint: "app_about_appSources_hint_a11y")
+        link(bugReportUrl, label: "app_about_bugReport_label", hint: "app_about_bugReport_hint_a11y")
+        link(designSystemUrl, label: "app_about_designSystem_label", hint: "app_about_designSystem_hint_a11y")
+    }
+
+    @ViewBuilder
+    private func link(_ url: URL, label: String, hint: String) -> some View {
+        Link(destination: url) {
+            HStack {
+                Text(label.localized())
+                Spacer()
+                if layoutDirection == .leftToRight {
+                    Image(systemName: "arrow.up.right.square").accessibilityHidden(true)
+                } else {
+                    Image(systemName: "arrow.up.left.square").accessibilityHidden(true)
+                }
+            }
+        }
+        .accessibilityHint(hint.localized())
+        .accessibilityRemoveTraits([.isButton]) // Has also link trait
     }
 }
