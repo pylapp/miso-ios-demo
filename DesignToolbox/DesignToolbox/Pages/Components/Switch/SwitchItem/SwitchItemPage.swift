f3a29b69e40f3d79c1deb3e347234c5a9008c2ba
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-07-02 16:30:06 +0200

    chore: remove duplicate folder

--- DesignToolbox/DesignToolbox/Pages/Components/Switch/SwitchItem/SwitchItemPage.swift
+++ DesignToolbox/DesignToolbox/Pages/Components/Switch/SwitchItem/SwitchItemPage.swift
@@ -51,7 +51,7 @@ private struct SwitchItemDemo: View {
                        isReadOnly: configurationModel.isReadOnly,
                        hasDivider: configurationModel.divider)
             .disabled(!configurationModel.enabled)
-            .padding(.all, theme.spaces.spaceFixedMedium)
+            .padding(.all, theme.spaces.spaceFixedMd)
             .accessibilityIdentifier(A11YIdentifiers.componentSwitchItem)
     }
 
