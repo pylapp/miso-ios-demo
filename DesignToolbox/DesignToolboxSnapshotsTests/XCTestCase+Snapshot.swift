29102982b8aa34e7a08dc6baa438f708b8784667
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-07-16 10:36:54 +0200

    test: custom precision and perceptual precision for some color tests (Orange-OpenSource/ouds-ios#851)
    Adjust precision and perceptual precision for some tests.
    There are some failures with two tokens, even if the snapshots are fresh:
    - Soth Theme, dark mode --> colorRepositoryOpacityBlackHighest
    - Soth Theme, dark mode --> colorSurfaceStatusNeutralEmphasized
    
    Closes Orange-OpenSource/ouds-ios#851
    
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- DesignToolbox/DesignToolboxSnapshotsTests/XCTestCase+Snapshot.swift
+++ DesignToolbox/DesignToolboxSnapshotsTests/XCTestCase+Snapshot.swift
@@ -19,12 +19,12 @@ import XCTest
 extension XCTestCase {
 
     /// Determines the percentage of pixels that must match exactly
-    private static let precision: Float = 0.95
+    public static let precision: Float = 0.96
 
     /// Determines the percentage of pixels that must match
     /// but adjusted for minor visual differences that may not be noticeable to the human eye,
     /// thus reducing false positive
-    private static let perceptualPrecision: Float = 0.97
+    public static let perceptualPrecision: Float = 0.97
 
     /// TImeout before failing the test
     private static let timeout: TimeInterval = 10
@@ -38,12 +38,15 @@ extension XCTestCase {
     ///    - uiStyle: The UI style to apply in `UITraitCollection` to compute the image, light mode or dark mode
     ///    - a11yContrast: The constrat mode, default set to normal
     ///    - named: Name of the snapshot image
+    ///    - precision: Percentage of pixels that must match exactly
+    ///    - perceptualPrecision: Percentage if pixels but adjusted for minor visual difference
     ///    - testName: Name for the tests
     @MainActor func assertIllustration(_ illustration: some View,
                                        on uiStyle: UIUserInterfaceStyle,
                                        a11yContrast: UIAccessibilityContrast = .normal,
                                        named: String,
                                        precision: Float = XCTestCase.precision,
+                                       perceptualPrecision: Float = XCTestCase.perceptualPrecision,
                                        testName: String,
                                        fileID: StaticString = #fileID,
                                        file: StaticString = #filePath)
@@ -51,7 +54,7 @@ extension XCTestCase {
         let hostingViewController = UIHostingController(rootView: illustration)
         assertSnapshot(of: hostingViewController,
                        as: .image(precision: precision,
-                                  perceptualPrecision: Self.perceptualPrecision,
+                                  perceptualPrecision: perceptualPrecision,
                                   traits: UITraitCollection(traitsFrom: [UITraitCollection(userInterfaceStyle: uiStyle), UITraitCollection(accessibilityContrast: a11yContrast)])),
                        named: named,
                        timeout: Self.timeout,
@@ -59,36 +62,5 @@ extension XCTestCase {
                        file: file,
                        testName: testName)
     }
-
     // swiftlint:enable function_default_parameter_at_end
-
-    /// Given the `UIHostingController` for a specific `UIUserInterfaceStyle`, make assertions on snapshots
-    /// Capture the snapshot of the illustration with the correct user interface style and save it with the snapshot name.
-    ///
-    /// - Parameters:
-    ///    - illustration: A value to compare against a reference,  embeded inside a `UIHostingControler` as a root view
-    ///    - uiStyle: The UI style to apply in `UITraitCollection` to compute the image
-    ///    - named: Some description for the test
-    ///    - precision: Percentage of pixels that must match exactly
-    ///    - percptualPrecision: Percentage if pixels but adjusted for minor visual difference
-    @MainActor static func assertIllustration(_ illustration: some View,
-                                              on uiStyle: UIUserInterfaceStyle,
-                                              named: String,
-                                              precision: Float,
-                                              perceptualPrecision: Float,
-                                              testName: String,
-                                              fileID: StaticString = #fileID,
-                                              file: StaticString = #filePath)
-    {
-        let hostingViewController = UIHostingController(rootView: illustration)
-        assertSnapshot(of: hostingViewController,
-                       as: .image(precision: precision,
-                                  perceptualPrecision: perceptualPrecision,
-                                  traits: UITraitCollection(userInterfaceStyle: uiStyle)),
-                       named: named,
-                       timeout: Self.timeout,
-                       fileID: fileID,
-                       file: file,
-                       testName: testName)
-    }
 }
